a19a7b3c9c461460344a7b7ef0db12a7f765d1d0
Wisdom Ojimah <76840699+detachedsoul@users.noreply.github.com>
2022-01-14 01:46:34 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 let dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://poetselixir.netlify.app';
+export const server = dev ? 'http://localhost:3000' : '';
